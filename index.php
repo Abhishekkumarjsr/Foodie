78e62c5563b4baf6569d179e330662fd6c3f2dd5
Abhishek <ak8002494585@gmail.com>
2024-03-12 15:37:42 +0530

    added web logo

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Foodie</title>
     <link rel="stylesheet" href="index.css">
+    <link rel="shortcut icon" type="x-icon" href="logo.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <script src="https://kit.fontawesome.com/ba586748f0.js" crossorigin="anonymous"></script>

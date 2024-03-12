b17fe34d00dcc6f6c7643c771e938da8ba309383
Abhishek <ak8002494585@gmail.com>
2024-03-12 14:59:51 +0530

    add all dishes to chowmein page

---
+++ chowmein.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foodie</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <script src="https://kit.fontawesome.com/ba586748f0.js" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+    <!-- header sectoin starts -->
+    <header class="navbar">
+        <div class="logo">
+            <a href="#"><img src="logo.png" alt="">
+                <h2>Foodie</h2>
+            </a>
+        </div>
+        <div class="link">
+            <a href="index.php">Home</a>
+            <a href="#" class="active">Dishes</a>
+            <a href="#">About</a>
+            <a href="#">Menu</a>
+            <a href="#">Review</a>
+            <a href="#">Orders</a>
+        </div>
+        <div class="symbols">
+            <a href="#"> <i class="material-symbols-outlined" id="search-label">search</i></a>
+            <a href="#"> <i class="material-symbols-outlined">favorite</i></a>
+            <a href="#"> <i class="material-symbols-outlined">shopping_cart</i></a>
+            <a href="#"> <i id="menu-bar" class="material-symbols-outlined menu">menu</i></a>
+        </div>
+    </header>
+    <!-- header sectoin ends -->
+
+    <!-- search form starts -->
+    <form action="" id="search-form">
+        <input type="search" placeholder="search here..." name="" id="search-box">
+        <label for="search-box"><span class="material-symbols-outlined">search</span></label>
+        <span class="material-symbols-outlined" id="close">close</span>
+    </form>
+    <!-- search form ends -->
+
+    <!-- Momos section starts -->
+    <section class="dishes" id="dishes">
+        <h3 class="sub-heading"> Momos </h3>
+        <h1 class="heading">Our Popular Momos Dishes </h1>
+        <div class="box-container">
+            <div class="box">
+                <img src="img/2.jpgS" alt="">
+                <div class="content">
+                    <a href="#" class="fas fa-heart"></a>
+                    <a href="#" class="fas fa-eye"></a>
+                    <h3>Chowmein</h3>
+                    <p>Veg Chow Mein</p>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                </div>
+                <span><strong>₹80.00 only</strong></span>
+                <div class="cart">
+                    <a href="#" class="btn">add to cart</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="img/2.jpg" alt="">
+                <div class="content">
+                    <a href="#" class="fas fa-heart"></a>
+                    <a href="#" class="fas fa-eye"></a>
+                    <h3>Chowmein</h3>
+                    <p>Chicken Chow Mein</p>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                </div>
+                <span><strong>₹75.00 only</strong></span>
+                <div class="cart">
+                    <a href="#" class="btn">add to cart</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="img/2.jpg" alt="">
+                <div class="content">
+                    <a href="#" class="fas fa-heart"></a>
+                    <a href="#" class="fas fa-eye"></a>
+                    <h3>Chowmein</h3>
+                    <p>Shrimp Chow Mein</p>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                </div>
+                <span><strong>₹75.00 only</strong></span>
+                <div class="cart">
+                    <a href="#" class="btn">add to cart</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="img/2.jpg" alt="">
+                <div class="content">
+                    <a href="#" class="fas fa-heart"></a>
+                    <a href="#" class="fas fa-eye"></a>
+                    <h3>Chowmein</h3>
+                    <p>Singapore Chow Mein</p>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                </div>
+                <span><strong>₹75.00 only</strong></span>
+                <div class="cart">
+                    <a href="#" class="btn">add to cart</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="img/2.jpg" alt="">
+                <div class="content">
+                    <a href="#" class="fas fa-heart"></a>
+                    <a href="#" class="fas fa-eye"></a>
+                    <h3>Chowmein</h3>
+                    <p>Hong Kong Style Chow Mein</p>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                </div>
+                <span><strong>₹75.00 only</strong></span>
+                <div class="cart">
+                    <a href="#" class="btn">add to cart</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="img/2.jpg" alt="">
+                <div class="content">
+                    <a href="#" class="fas fa-heart"></a>
+                    <a href="#" class="fas fa-eye"></a>
+                    <h3>Chowmein</h3>
+                    <p>Szechuan Chow Mein</p>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                </div>
+                <span><strong>₹75.00 only</strong></span>
+                <div class="cart">
+                    <a href="#" class="btn">add to cart</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="img/2.jpg" alt="">
+                <div class="content">
+                    <a href="#" class="fas fa-heart"></a>
+                    <a href="#" class="fas fa-eye"></a>
+                    <h3>Chowmein</h3>
+                    <p>Seafood Chow Mein</p>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                </div>
+                <span><strong>₹75.00 only</strong></span>
+                <div class="cart">
+                    <a href="#" class="btn">add to cart</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="img/2.jpg" alt="">
+                <div class="content">
+                    <a href="#" class="fas fa-heart"></a>
+                    <a href="#" class="fas fa-eye"></a>
+                    <h3>Chowmein</h3>
+                    <p>Crispy Chow Mein</p>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                </div>
+                <span><strong>₹75.00 only</strong></span>
+                <div class="cart">
+                    <a href="#" class="btn">add to cart</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="img/2.jpg" alt="">
+                <div class="content">
+                    <a href="#" class="fas fa-heart"></a>
+                    <a href="#" class="fas fa-eye"></a>
+                    <h3>Chowmein</h3>
+                    <p>Spicy Garlic Chow Mein</p>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                </div>
+                <span><strong>₹75.00 only</strong></span>
+                <div class="cart">
+                    <a href="#" class="btn">add to cart</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="img/2.jpg" alt="">
+                <div class="content">
+                    <a href="#" class="fas fa-heart"></a>
+                    <a href="#" class="fas fa-eye"></a>
+                    <h3>Chowmein</h3>
+                    <p>Satay Chow Mein</p>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                </div>
+                <span><strong>₹75.00 only</strong></span>
+                <div class="cart">
+                    <a href="#" class="btn">add to cart</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="img/2.jpg" alt="">
+                <div class="content">
+                    <a href="#" class="fas fa-heart"></a>
+                    <a href="#" class="fas fa-eye"></a>
+                    <h3>Chowmein</h3>
+                    <p>Cashew Nut Chow Mein</p>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                </div>
+                <span><strong>₹75.00 only</strong></span>
+                <div class="cart">
+                    <a href="#" class="btn">add to cart</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- dishes section ends -->
+
+    <script src="index.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+</body>
+
+</html>
